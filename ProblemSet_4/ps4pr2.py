9d8dcd552cfb0226cff667cb9d4e87b503352304
Tigran Melkonian <tigran96@bu.edu>
2019-02-12 09:19:46 -0500

    Update ps4pr2.py

--- ProblemSet_4/ps4pr2.py
+++ ProblemSet_4/ps4pr2.py
@@ -32,25 +32,3 @@ def add_bytes(b1, b2):
     else:
         return bin_num
 
-
-def test():
-    """test functions above"""
-
-    print(dec_to_bin(4))
-    print(dec_to_bin(7))
-    print(dec_to_bin(10))
-    print(dec_to_bin(111))
-    print(dec_to_bin(100))
-    print(bin_to_dec('1110'))
-    print(bin_to_dec('1100'))
-    print(bin_to_dec('00011010'))
-    print(bin_to_dec('111'))
-    print(add_bytes('10000011', '11000001'))
-    print(add_bytes('00011100', '00011110'))
-    print(add_bytes('00000011', '00000001'))
-    print(mul_bin('11', '10'))
-    print(mul_bin('1001', '101'))
-
-
-test()
-
